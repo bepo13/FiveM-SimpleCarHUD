0425d1a66dead15281762e25a8019525232cc21f
bepo13 <bepo13@gmail.com>
2019-06-15 10:05:24 -0700

    Performance optimizations, 4x faster

--- SimpleCarHUD/SimpleCarHUD_cl.lua
+++ SimpleCarHUD/SimpleCarHUD_cl.lua
@@ -16,8 +16,8 @@ local fuelColorUnder = {255, 96, 96}        -- Color used to display fuel warnin
 
 -- SEATBELT PARAMETERS
 local seatbeltInput = 311                   -- Toggle seatbelt on/off with K
-local seatbeltEjectSpeed = 45               -- Speed threshold to eject player (MPH)
-local seatbeltEjectAccel = 100              -- Acceleration threshold to eject player (G's)
+local seatbeltEjectSpeed = 45.0             -- Speed threshold to eject player (MPH)
+local seatbeltEjectAccel = 100.0            -- Acceleration threshold to eject player (G's)
 local seatbeltColorOn = {160, 255, 160}     -- Color used when seatbelt is on
 local seatbeltColorOff = {255, 96, 96}      -- Color used when seatbelt is off
 
@@ -33,9 +33,11 @@ local locationColorText = {255, 255, 255}   -- Color used to display location st
 local directions = { [0] = 'N', [1] = 'NW', [2] = 'W', [3] = 'SW', [4] = 'S', [5] = 'SE', [6] = 'E', [7] = 'NE', [8] = 'N' } 
 local zones = { ['AIRP'] = "Los Santos International Airport", ['ALAMO'] = "Alamo Sea", ['ALTA'] = "Alta", ['ARMYB'] = "Fort Zancudo", ['BANHAMC'] = "Banham Canyon Dr", ['BANNING'] = "Banning", ['BEACH'] = "Vespucci Beach", ['BHAMCA'] = "Banham Canyon", ['BRADP'] = "Braddock Pass", ['BRADT'] = "Braddock Tunnel", ['BURTON'] = "Burton", ['CALAFB'] = "Calafia Bridge", ['CANNY'] = "Raton Canyon", ['CCREAK'] = "Cassidy Creek", ['CHAMH'] = "Chamberlain Hills", ['CHIL'] = "Vinewood Hills", ['CHU'] = "Chumash", ['CMSW'] = "Chiliad Mountain State Wilderness", ['CYPRE'] = "Cypress Flats", ['DAVIS'] = "Davis", ['DELBE'] = "Del Perro Beach", ['DELPE'] = "Del Perro", ['DELSOL'] = "La Puerta", ['DESRT'] = "Grand Senora Desert", ['DOWNT'] = "Downtown", ['DTVINE'] = "Downtown Vinewood", ['EAST_V'] = "East Vinewood", ['EBURO'] = "El Burro Heights", ['ELGORL'] = "El Gordo Lighthouse", ['ELYSIAN'] = "Elysian Island", ['GALFISH'] = "Galilee", ['GOLF'] = "GWC and Golfing Society", ['GRAPES'] = "Grapeseed", ['GREATC'] = "Great Chaparral", ['HARMO'] = "Harmony", ['HAWICK'] = "Hawick", ['HORS'] = "Vinewood Racetrack", ['HUMLAB'] = "Humane Labs and Research", ['JAIL'] = "Bolingbroke Penitentiary", ['KOREAT'] = "Little Seoul", ['LACT'] = "Land Act Reservoir", ['LAGO'] = "Lago Zancudo", ['LDAM'] = "Land Act Dam", ['LEGSQU'] = "Legion Square", ['LMESA'] = "La Mesa", ['LOSPUER'] = "La Puerta", ['MIRR'] = "Mirror Park", ['MORN'] = "Morningwood", ['MOVIE'] = "Richards Majestic", ['MTCHIL'] = "Mount Chiliad", ['MTGORDO'] = "Mount Gordo", ['MTJOSE'] = "Mount Josiah", ['MURRI'] = "Murrieta Heights", ['NCHU'] = "North Chumash", ['NOOSE'] = "N.O.O.S.E", ['OCEANA'] = "Pacific Ocean", ['PALCOV'] = "Paleto Cove", ['PALETO'] = "Paleto Bay", ['PALFOR'] = "Paleto Forest", ['PALHIGH'] = "Palomino Highlands", ['PALMPOW'] = "Palmer-Taylor Power Station", ['PBLUFF'] = "Pacific Bluffs", ['PBOX'] = "Pillbox Hill", ['PROCOB'] = "Procopio Beach", ['RANCHO'] = "Rancho", ['RGLEN'] = "Richman Glen", ['RICHM'] = "Richman", ['ROCKF'] = "Rockford Hills", ['RTRAK'] = "Redwood Lights Track", ['SANAND'] = "San Andreas", ['SANCHIA'] = "San Chianski Mountain Range", ['SANDY'] = "Sandy Shores", ['SKID'] = "Mission Row", ['SLAB'] = "Stab City", ['STAD'] = "Maze Bank Arena", ['STRAW'] = "Strawberry", ['TATAMO'] = "Tataviam Mountains", ['TERMINA'] = "Terminal", ['TEXTI'] = "Textile City", ['TONGVAH'] = "Tongva Hills", ['TONGVAV'] = "Tongva Valley", ['VCANA'] = "Vespucci Canals", ['VESP'] = "Vespucci", ['VINE'] = "Vinewood", ['WINDF'] = "Ron Alternates Wind Farm", ['WVINE'] = "West Vinewood", ['ZANCUDO'] = "Zancudo River", ['ZP_ORT'] = "Port of South Los Santos", ['ZQ_UAR'] = "Davis Quartz" }
 
--- STATE VARIABLES
-local cruiseIsOn = false
-local seatbeltIsOn = false
+-- Globals
+local pedInVeh = false
+local timeText = ""
+local locationText = ""
+local currentFuel = 0.0
 
 -- Main thread
 Citizen.CreateThread(function()
@@ -43,6 +45,8 @@ Citizen.CreateThread(function()
     local currSpeed = 0.0
     local cruiseSpeed = 999.0
     local prevVelocity = {x = 0.0, y = 0.0, z = 0.0}
+    local cruiseIsOn = false
+    local seatbeltIsOn = false
 
     while true do
         -- Loop forever and update HUD every frame
@@ -55,21 +59,13 @@ Citizen.CreateThread(function()
         
         -- Display Location and time when in any vehicle (including bicycles)
         if (IsPedInAnyVehicle(player, false)) then
+            -- Set vehicle state
+            pedInVeh = true
+
             -- Get time and display
-            local hour = GetClockHours()
-            local minute = GetClockMinutes()
-            local timeText = ("%.2d"):format((hour == 0) and 12 or hour) .. ":" .. ("%.2d"):format( minute) .. ((hour < 12) and " AM" or " PM")
             drawTxt(timeText, 4, {255,255,255}, 0.4, screenPosX, screenPosY + 0.048)
             
-            -- Get heading and zone from lookup tables and street name from hash
-            local heading = directions[math.floor((GetEntityHeading(player) + 22.5) / 45.0)]
-            local zoneNameFull = zones[GetNameOfZone(position.x, position.y, position.z)]
-            local streetName = GetStreetNameFromHashKey(GetStreetNameAtCoord(position.x, position.y, position.z))
-            
             -- Display heading, street name and zone when possible
-            local locationText = heading
-            locationText = (streetName == "" or streetName == nil) and (locationText) or (locationText .. " | " .. streetName)
-            locationText = (zoneNameFull == "" or zoneNameFull == nil) and (locationText) or (locationText .. " | " .. zoneNameFull)
             drawTxt(locationText, 4, locationColorText, 0.5, screenPosX, screenPosY + 0.075)
         
             -- Display remainder of HUD when engine is on and vehicle is not a bicycle
@@ -121,8 +117,7 @@ Citizen.CreateThread(function()
                 drawTxt(("%.3d"):format(math.ceil(speed)), 2, speedColor, 0.8, screenPosX + 0.000, screenPosY + 0.000)
                 drawTxt("MPH", 2, speedColorText, 0.4, screenPosX + 0.030, screenPosY + 0.018)
                 
-                -- Draw fuel gauge; always displays 100 but can be modified by setting currentFuel with an API call
-                local currentFuel = GetVehicleFuelLevel(vehicle)
+                -- Draw fuel gauge
                 local fuelColor = (currentFuel >= fuelWarnLimit) and fuelColorOver or fuelColorUnder
                 drawTxt(("%.3d"):format(math.ceil(currentFuel)), 2, fuelColor, 0.8, screenPosX + 0.055, screenPosY + 0.000)
                 drawTxt("FUEL", 2, fuelColorText, 0.4, screenPosX + 0.085, screenPosY + 0.018)
@@ -137,8 +132,46 @@ Citizen.CreateThread(function()
             end
         else
             -- Reset states when not in car
+            pedInVeh = false
             cruiseIsOn = false
             seatbeltIsOn = false
+        end
+    end
+end)
+
+-- Secondary thread to update strings
+Citizen.CreateThread(function()
+    while true do
+        -- Update when player is in a vehicle
+        if pedInVeh then
+            -- Get player, position and vehicle
+            local player = GetPlayerPed(-1)
+            local position = GetEntityCoords(player)
+            local vehicle = GetVehiclePedIsIn(player, false)
+
+            -- Update fuel
+            currentFuel = GetVehicleFuelLevel(vehicle)
+
+            -- Update time text string
+            local hour = GetClockHours()
+            local minute = GetClockMinutes()
+            timeText = ("%.2d"):format((hour == 0) and 12 or hour) .. ":" .. ("%.2d"):format( minute) .. ((hour < 12) and " AM" or " PM")
+
+            -- Get heading and zone from lookup tables and street name from hash
+            local heading = directions[math.floor((GetEntityHeading(player) + 22.5) / 45.0)]
+            local zoneNameFull = zones[GetNameOfZone(position.x, position.y, position.z)]
+            local streetName = GetStreetNameFromHashKey(GetStreetNameAtCoord(position.x, position.y, position.z))
+            
+            -- Update location text string
+            locationText = heading
+            locationText = (streetName == "" or streetName == nil) and (locationText) or (locationText .. " | " .. streetName)
+            locationText = (zoneNameFull == "" or zoneNameFull == nil) and (locationText) or (locationText .. " | " .. zoneNameFull)
+
+            -- Update every second
+            Citizen.Wait(1000)
+        else
+            -- Wait until next frame
+            Citizen.Wait(0)
         end
     end
 end)
